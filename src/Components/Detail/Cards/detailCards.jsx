a773e9d00bb4c4012131b7e598545b2e41e52c3e
Decraene Robin <robin.decraene@student.arteveldehs.be>
2023-09-17 20:48:35 +0200

    commit

--- src/Components/Detail/Cards/detailCards.jsx
+++ src/Components/Detail/Cards/detailCards.jsx
@@ -1,23 +1,38 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
 import style from './detailCards.module.css';
+import Loading from '../../Global/Loading/loading';
 
-const Detailcards = ({demoData}) => {
+const Detailcards = () => {
+  const { id } = useParams();
+  const [demoData, setDemoData] = useState(null);
 
-  if(!demoData) return '';
-    else return (
-        <section className={style.detailCard}>
-            <div className={style.infoDetail}>
-                <img className={style.imgDetail} src={demoData.image} alt="img" />
-                <div className={style.tekstDetail}>
-                    <p className={style.titleDetail}>{demoData.title}</p>
-                    <p className={style.descriptionDetail}>{demoData.blog_paragrafe}</p>
-                    <p className={style.dateDetail}>{demoData.datum}</p>
-                </div>
-                
-            </div>
-        </section>
-    
-    );
+  useEffect(() => {
+    const fetchDemoData = async () => {
+      
+        const response = await fetch('../barcelonaBlog.json');
+        const data = await response.json();
+        const parsedId = parseInt(id, 10);
+        const detailItem = data.find((item) => item.id === parsedId);
+        setDemoData(detailItem);
+    };
+
+    fetchDemoData();
+  }, [id]);
+
+  if (!demoData) return (<Loading/>);
+
+  return (
+    <section className={style.detailCard}>
+        <img className={style.imgDetail} src={demoData.image} alt="img" />
+        <div className={style.tekstDetail}>
+          <p className={style.titleDetail}>{demoData.title}</p>
+          <p className={style.descriptionDetail}>{demoData.blog_paragrafe}</p>
+          <p className={style.dateDetail}>{demoData.datum}</p>
+        </div>
+      
+    </section>
+  );
 };
 
 export default Detailcards;
